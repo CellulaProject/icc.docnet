27ee61b509cef32486017f32c7f01f90fa708d70
Evgeny Cherkashin <eugene@irnok.net>
2017-08-18 17:48:24 +0800

    Added upgate script.

--- update.sh
+++ update.sh
@@ -4,10 +4,13 @@
 for d in `find . -name 'i*' -type d`
 do
     # echo "searching in $d"
+    pushd $d
     if [ -d .git ]; then
        echo "Updating $d"
-       (cd $d && git pull origin master)
+       git pull $*
+       git submodule update
     fi
+    popd
 done
 #(cd isu.college && touch ./pyramid.wsgi)
 #sudo chown eugeneai:users -R *
